da57bbc8f094b9212adc205f012bf6511c3e5204
mauroquinteros <quinterosmauro0599@gmail.com>
2020-08-24 16:33:12 -0500

    Adding email routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 // Settings
 const port = process.env.PORT || 3000;
 app.use(cors());
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const router = Router();
+const nodemailer = require("nodemailer");
+
+const { emails } = JSON.parse(process.env.EMAILS);
 
 router.get("/", (req, res) => {
   res.json({
     message: "Hello World",
     app: "Coder Gods",
+    email: process.env.MAILER,
   });
 });
 
@@ -14,6 +18,65 @@ router.post("/users", (req, res) => {
     data,
     date: new Date(),
   });
+});
+router.post("/send", (req, res) => {
+  try {
+    const { userName, userEmail, userMessage } = req.body;
+
+    contentMessage = `
+      <h1>User Information</h1>
+      <ul>
+        <li>User Name: ${userName}</li>
+        <li>User Email: ${userEmail}</li>
+        <li>User Message: ${userMessage}</li>
+      </ul>
+    `;
+
+    const transporter = nodemailer.createTransport({
+      host: "mail.privateemail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAILER,
+        pass: process.env.PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    transporter.sendMail(
+      {
+        from: `${process.env.NAME} <${process.env.MAILER}>`,
+        to: [...emails],
+        subject: "Contacto CoderGods",
+        html: contentMessage,
+      },
+      (err, info) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            success: false,
+            message: "Lo siento, inténtelo más tarde.",
+          });
+        } else {
+          console.log(info);
+          res.status(201).json({
+            email_id: info.messageId,
+            success: true,
+            message:
+              "Gracias por contactarte con nosotros,te responderemos pronto.",
+          });
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Lo siento, inténtelo más tarde.",
+    });
+  }
 });
 
 module.exports = router;
